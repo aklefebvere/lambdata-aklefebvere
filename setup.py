188cbd1812bc416a99f09d45594f6a0fac72b821
aklefebvere <aklefebvere@gmail.com>
2020-03-09 18:17:35 -0400

    Updated setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="lambdata-aklefebvere",
-    version="1.0",
+    version="1.1",
     author="aklefebvere",
     author_email="aklefebvere@gmail.com",
     description="This is an example",
